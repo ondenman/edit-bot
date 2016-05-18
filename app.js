4246ccbc86b34a94a43e6bd340a8f7aa2b546062
Oliver Denman <oliver@therewillbeanimals.com>
2016-05-18 17:42:44 +0100

    Added error checking for URLs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const request = require('request')
+const requestSync = require('sync-request')
 const twitter = require('twitter')
 const auth = require('./auth.js')
 const data = require('./data-functions.js')
@@ -18,22 +19,42 @@ const client = new twitter ({
 
 const queryStart = "https://en.wikipedia.org/w/api.php?action=query&titles="
 const queryEnd = "&prop=revisions&rvlimit=500&format=json"
+const interval = conf.interval * 1000
+
+;(function main() {
+    checkArticles()
+    console.log("Bot running "+date())
+    fireTimer()
+    // tweet('Bot running. '+date())
+})()
+
+function checkArticles() {
+    articles.forEach((article) => {
+        checkURL(composeQuery(article))
+    })
+}
+
+function checkURL(url){
+    const urlCheck = requestSync('GET', url)
+    try {
+        if (JSON.parse(urlCheck.getBody('utf8')).batchcomplete === '') {
+            console.log('There is a problem with this URL:\r\n'+url)
+            throw new Error('Check article names in conf.json')
+        }
+    }
+    catch(error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
 
 function date(){
     return new Date(Date.now())
 }
 
-function formQuery(name) {
-    return queryStart+name+queryEnd
+function composeQuery(articleName) {
+    return queryStart+articleName+queryEnd
 }
-
-const interval = conf.interval * 1000
-
-;(function main() {
-    console.log("Bot running "+date())
-    fireTimer()
-    // tweet('Bot running. '+date())
-})()
 
 function fireTimer() {
     setInterval(function(){
@@ -44,7 +65,7 @@ function fireTimer() {
 
 function checkWikipedia() {
     articles.forEach((name) => {
-        request({url:formQuery(name), encoding:"utf8"}, (err, res, body) => {
+        request({url:composeQuery(name), encoding:"utf8"}, (err, res, body) => {
             if (!err) {
                 let revisions = data.getRevisions(body)
                 data.scrapeData(revisions, name)
